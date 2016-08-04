a301920bc7ac733482dc339e307cab0106a65a8e
mika-nt28 <michaelhalbout@gmail.com>
2016-08-04 08:39:54 +0200

    Add files via upload

---
+++ Jeedom/Api/Json/Response/Response.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Jeedom.Api.Json.Response
+{
+    [DataContract]
+    public class Response<T>
+    {
+        [DataMember]
+        public string id;
+
+        [DataMember]
+        public string jsonrpc;
+
+        [DataMember]
+        public T result;
+    }
+}
---
+++ Jeedom/Api/Json/Response/ResponseError.cs
@@ -0,0 +1,18 @@
+﻿using Jeedom.Model;
+using System.Runtime.Serialization;
+
+namespace Jeedom.Api.Json.Response
+{
+    [DataContract]
+    public class ResponseError
+    {
+        [DataMember]
+        public string id;
+
+        [DataMember]
+        public string jsonrpc;
+
+        [DataMember]
+        public Error error;
+    }
+}
