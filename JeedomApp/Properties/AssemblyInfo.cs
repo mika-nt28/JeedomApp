174dc12a2a678a4627014e3e41a96687af1421f2
halbout michael <michaelhalbout@outlook.fr>
2016-08-16 08:36:37 +0200

    Mise a jours

--- JeedomApp/Properties/AssemblyInfo.cs
+++ JeedomApp/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JeedomApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Application non officiel pour controler votre maison avec le serveur domotique Jeedom")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JeedomApp")]
@@ -27,3 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("")]
+
