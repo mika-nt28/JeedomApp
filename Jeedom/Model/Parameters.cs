facde8939213fe4d9b355814eea4fffda716ae97
Halbout Michael <michaelhalbout@gmail.com>
2016-12-02 08:14:26 +0100

    Update Parameters.cs

--- Jeedom/Model/Parameters.cs
+++ Jeedom/Model/Parameters.cs
@@ -7,26 +7,24 @@ namespace Jeedom.Model
     {
         [DataMember]
         public string apikey;
-
         [DataMember(IsRequired = false)]
         public string eqLogic_id;
-
         [DataMember]
         public string id;
-
         [DataMember]
         public string name;
         [DataMember]
         public string query;
         [DataMember]
+        public string plugin;
+        [DataMember]
+        public string platform;
+        [DataMember]
         public ParametersOption options;
-        
         [DataMember(IsRequired = false)]
         public string state;
-
         [DataMember(IsRequired = false)]
         public string object_id;
-
         [DataMember(IsRequired = false)]
         public double datetime;
     }
