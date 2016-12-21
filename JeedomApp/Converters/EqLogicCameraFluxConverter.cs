63de44fb6d5784d1c2ee5d2555c49b9a58a5c32b
DanoneKiD <simonj@hotmail.fr>
2016-12-21 05:35:25 +0100

    Ajout d'un simple converter pour le flux caméra

---
+++ JeedomApp/Converters/EqLogicCameraFluxConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace JeedomApp.Converters
+{
+    internal class EqLogicCameraFluxConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var url = value as string;
+            if (url != null)
+                return Jeedom.RequestViewModel.config.Uri + url;
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
