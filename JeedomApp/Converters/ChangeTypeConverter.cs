95a4bfe9a1e9236be953992fcc25402028f9e6ac
DanoneKiD <simonj@hotmail.fr>
2017-02-15 11:22:11 +0100

    Ajout d'un converter de changement de Type et de Formatage

---
+++ JeedomApp/Converters/ChangeTypeConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace JeedomApp.Converters
+{
+    public class ChangeTypeConverter : IValueConverter
+    {
+        #region Public Properties
+
+        public string OutType { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var _outType = OutType;
+            if (_outType == null)
+                return value;
+
+            switch (OutType)
+            {
+                case "Double":
+                    Double _double;
+                    Double.TryParse((string)value, out _double);
+                    return _double;
+
+                default:
+                    return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ JeedomApp/Converters/StringFormatConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+namespace JeedomApp.Converters
+{
+
+        public class StringFormatConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, string language)
+            {
+                var format = Format;
+                if (format == null)
+                    return value;
+
+                if (string.IsNullOrWhiteSpace(language))
+                {
+                    var t = string.Format(format, value);
+                    return t;
+                }
+
+                try
+                {
+                    var culture = new CultureInfo(language);
+                    return string.Format(culture, format, value);
+                }
+                catch
+                {
+                    return string.Format(format, value);
+                }
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, string language)
+            {
+                throw new NotImplementedException();
+            }
+
+            public string Format { get; set; }
+        }
+    
+}
