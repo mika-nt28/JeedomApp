39b17fd33b88c91d99d7692d7fdfacc2d75cf83d
mika-nt28 <michaelhalbout@outlook.fr>
2017-04-23 10:38:55 +0200

    Refonte du widget volet

--- JeedomApp/Controls/ConnectDialog.xaml.cs
+++ JeedomApp/Controls/ConnectDialog.xaml.cs
@@ -1,22 +1,13 @@
 ﻿using Jeedom;
 using System;
-//using System.Diagnostics;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
 using System.Threading.Tasks;
 using Template10.Common;
 using Template10.Controls;
-using Windows.Devices.Enumeration;
-using Windows.Devices.PointOfService;
-using Windows.Foundation.Metadata;
-using Windows.Media.Capture;
-using Windows.Storage.Streams;
-using Windows.System.Display;
 using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
+using Windows.Media.SpeechRecognition;
 
 //TODO: Gérer l'adresse sur le dns jeedom
 
@@ -27,24 +18,19 @@ namespace JeedomApp.Controls
     /// </summary>
     public sealed partial class ConnectDialog : UserControl
     {
+        SpeechRecognizer recognizer;
         public ConnectDialog()
         {
             this.InitializeComponent();
         }
-        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void PivotConnection_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int no = Pivot.SelectedIndex; 
-            if(no == 1)  
+            if (PivotConnection.SelectedIndex == 1)
             {
-                ZXingQrCodeScanner.ScanFirstCameraForQrCode(
-                  result =>
-                  {
-                    // TODO: do something with the QR code result.
-                  },
-                  TimeSpan.FromSeconds(30));
+
             }
         }
-
+        
         private void Demo_Click(object sender, RoutedEventArgs e)
         {
             // Charger des données de demo pour l'application
@@ -52,7 +38,6 @@ namespace JeedomApp.Controls
             // Masque le dialogue de connection
             ConnectDialog.HideConnectDialog();
         }
-
         private async void ShowError(string message)
         {
             var dialog = new MessageDialog(message);
@@ -60,7 +45,7 @@ namespace JeedomApp.Controls
             return;
         }
 
-        private async void bConnect_Click(object sender, RoutedEventArgs e)
+       private async void bConnect_Click(object sender, RoutedEventArgs e)
         {
             // Lance le rapatriement des données de Jeedom
             var taskFactory = new TaskFactory(TaskScheduler.FromCurrentSynchronizationContext());
@@ -91,10 +76,6 @@ namespace JeedomApp.Controls
                 ConnectDialog.HideConnectDialog();
             });
         }
-
-        /// <summary>
-        /// Affiche le dialogue de Connection
-        /// </summary>
         public static void ShowConnectDialog()
         {
             WindowWrapper.Current().Dispatcher.Dispatch(() =>
@@ -107,10 +88,6 @@ namespace JeedomApp.Controls
                 //view.Logo.Begin();
             });
         }
-
-        /// <summary>
-        /// Masque le dialogue de Connection
-        /// </summary>
         public static void HideConnectDialog()
         {
             WindowWrapper.Current().Dispatcher.Dispatch(() =>
@@ -121,205 +98,6 @@ namespace JeedomApp.Controls
             });
         }
 
-        private BarcodeScanner scanner = null;
-        private ClaimedBarcodeScanner claimedScanner = null;
-
-        private async Task<bool> CreateDefaultScannerObject()
-        {
-            if (scanner == null)
-            {
-                var DeviceSelector = BarcodeScanner.GetDeviceSelector();
-                DeviceInformationCollection deviceCollection = await DeviceInformation.FindAllAsync(DeviceSelector);
-                if (deviceCollection != null && deviceCollection.Count > 0)
-                {
-                    foreach (var device in deviceCollection) { 
-                        scanner = await BarcodeScanner.FromIdAsync(device.Id);
-                        if (scanner != null)
-                            break;
-                    }
-                    if (scanner == null)
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private async Task<bool> ClaimScanner()
-        {
-            if (claimedScanner == null)
-            {
-                // claim the barcode scanner
-                claimedScanner = await scanner.ClaimScannerAsync();
-
-                // enable the claimed barcode scanner
-                if (claimedScanner == null)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private async void QrCodeInfo_Click(object sender, RoutedEventArgs e)
-        {
-            // create the barcode scanner.
-            if (await CreateDefaultScannerObject())
-            {
-                // after successful creation, claim the scanner for exclusive use and enable it so
-                // that data reveived events are received.
-                if (await ClaimScanner())
-                {
-                    // It is always a good idea to have a release device requested event handler. If
-                    // this event is not handled, there are chances of another app can claim
-                    // ownsership of the barcode scanner.
-                    claimedScanner.ReleaseDeviceRequested += claimedScanner_ReleaseDeviceRequested;
-
-                    // after successfully claiming, attach the datareceived event handler.
-                    claimedScanner.DataReceived += claimedScanner_DataReceived;
-
-                    // Ask the API to decode the data by default. By setting this, API will decode
-                    // the raw data from the barcode scanner and send the ScanDataLabel and
-                    // ScanDataType in the DataReceived event
-                    claimedScanner.IsDecodeDataEnabled = true;
-
-                    // enable the scanner.
-                    // Note: If the scanner is not enabled (i.e. EnableAsync not called), attaching
-                    //       the event handler will not be any useful because the API will not fire
-                    //       the event if the claimedScanner has not beed Enabled
-                    await claimedScanner.EnableAsync();
-
-                    // reset the button state
-                    //ScenarioEndScanButton.IsEnabled = true;
-                    // ScenarioStartScanButton.IsEnabled = false;
-                }
-            }
-        }
-
-        private async void claimedScanner_ReleaseDeviceRequested(object sender, ClaimedBarcodeScanner e)
-        {
-            // always retain the device
-            e.RetainDevice();
-
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-            });
-        }
-
-        private string GetDataString(IBuffer data)
-        {
-            StringBuilder result = new StringBuilder();
-
-            if (data == null)
-            {
-                result.Append("No data");
-            }
-            else
-            {
-                // Just to show that we have the raw data, we'll print the value of the bytes.
-                // Arbitrarily limit the number of bytes printed to 20 so the UI isn't overloaded.
-                const uint MAX_BYTES_TO_PRINT = 20;
-                uint bytesToPrint = Math.Min(data.Length, MAX_BYTES_TO_PRINT);
-
-                DataReader reader = DataReader.FromBuffer(data);
-                byte[] dataBytes = new byte[bytesToPrint];
-                reader.ReadBytes(dataBytes);
-
-                for (uint byteIndex = 0; byteIndex < bytesToPrint; ++byteIndex)
-                {
-                    result.AppendFormat("{0:X2} ", dataBytes[byteIndex]);
-                }
-
-                if (bytesToPrint < data.Length)
-                {
-                    result.Append("...");
-                }
-            }
-
-            return result.ToString();
-        }
-
-        private string GetDataLabelString(IBuffer data, uint scanDataType)
-        {
-            string result = null;
-            // Only certain data types contain encoded text. To keep this simple, we'll just decode a
-            // few of them.
-            if (data == null)
-            {
-                result = "No data";
-            }
-            else
-            {
-                switch (BarcodeSymbologies.GetName(scanDataType))
-                {
-                    case "Upca":
-                    case "UpcaAdd2":
-                    case "UpcaAdd5":
-                    case "Upce":
-                    case "UpceAdd2":
-                    case "UpceAdd5":
-                    case "Ean8":
-                    case "TfStd":
-                        // The UPC, EAN8, and 2 of 5 families encode the digits 0..9 which are then
-                        // sent to the app in a UTF8 string (like "01234")
-
-                        // This is not an exhaustive list of symbologies that can be converted to a string
-
-                        DataReader reader = DataReader.FromBuffer(data);
-                        result = reader.ReadString(data.Length);
-                        break;
-
-                    default:
-                        // Some other symbologies (typically 2-D symbologies) contain binary data
-                        // that should not be converted to text.
-                        result = string.Format("Decoded data unavailable. Raw label data: {0}", GetDataString(data));
-                        break;
-                }
-            }
-
-            return result;
-        }
-
-        private async void claimedScanner_DataReceived(ClaimedBarcodeScanner sender, BarcodeScannerDataReceivedEventArgs args)
-        {
-            // need to update the UI data on the dispatcher thread. update the UI with the data
-            // received from the scan.
-            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
-            {
-                // read the data from the buffer and convert to string.
-                // ScenarioOutputScanDataLabel.Text = GetDataLabelString(args.Report.ScanDataLabel, args.Report.ScanDataType);
-
-                // ScenarioOutputScanData.Text = GetDataString(args.Report.ScanData);
-
-                // ScenarioOutputScanDataType.Text = BarcodeSymbologies.GetName(args.Report.ScanDataType);
-            });
-        }
-
-        private void ResetTheScenarioState()
-        {
-            if (claimedScanner != null)
-            {
-                // Detach the event handlers
-                claimedScanner.DataReceived -= claimedScanner_DataReceived;
-                claimedScanner.ReleaseDeviceRequested -= claimedScanner_ReleaseDeviceRequested;
-                // Release the Barcode Scanner and set to null
-                claimedScanner.Dispose();
-                claimedScanner = null;
-            }
-
-            scanner = null;
-        }
-
-        /* private void ScenarioEndScanButton_Click(object sender, RoutedEventArgs e)
-         {
-             // reset the scenario state
-             this.ResetTheScenarioState();
-         }*/
+       
     }
 }
---
+++ JeedomApp/Controls/Plugins/VoletPluginControl.xaml.cs
@@ -0,0 +1,141 @@
+﻿using Jeedom.Model;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Template10.Mvvm;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace JeedomApp.Controls
+{
+    public sealed partial class VoletPluginControl : UserControl
+    {
+        #region Public Fields
+
+       
+        public static readonly DependencyProperty SilderProperty =
+            DependencyProperty.Register("FLAP_SLIDER", typeof(Command), typeof(VoletPluginControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DownProperty =
+            DependencyProperty.Register("FLAP_DOWN", typeof(Command), typeof(VoletPluginControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty StopProperty =
+             DependencyProperty.Register("FLAP_STOP", typeof(Command), typeof(VoletPluginControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty UpProperty =
+             DependencyProperty.Register("FLAP_UP", typeof(Command), typeof(VoletPluginControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty StateProperty =
+            DependencyProperty.Register("FLAP_STATE", typeof(string), typeof(VoletPluginControl), new PropertyMetadata(null));
+
+        #endregion Public Fields
+
+        #region Private Fields
+
+        private bool _DataContextLoaded = false;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public VoletPluginControl()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += VoletPluginControl_DataContextChanged;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Public Events
+
+        #region Public Properties
+
+        public Command Position { get { return (Command)GetValue(SilderProperty); } set { SetValue(SilderProperty, value); } }
+        public Command Down { get { return (Command)GetValue(DownProperty); } set { SetValue(DownProperty, value); } }
+        public Command Stop { get { return (Command)GetValue(StopProperty); } set { SetValue(StopProperty, value); } }
+        public Command Up { get { return (Command)GetValue(UpProperty); } set { SetValue(UpProperty, value); } }
+        public string State { get { return (string)GetValue(StateProperty); } set { SetValue(StateProperty, value); } }
+       
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null) =>
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(storage, value))
+            {
+                storage = value;
+                RaisePropertyChanged(propertyName);
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private void Cmds_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+
+        }
+
+        private void VoletPluginControl_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+            //System.Diagnostics.Debug.WriteLine("DataContexChange");
+            // Pas besoin de recharger
+            if (_DataContextLoaded)
+                return;
+
+            if (this.DataContext == null)
+                return;
+            if (!(this.DataContext.GetType() == typeof(EqLogic)))
+                return;
+
+            var eq = (EqLogic)this.DataContext;
+
+            // On s'abonne aux changements des commandes
+            eq.Cmds.CollectionChanged += Cmds_CollectionChanged; ;
+
+            foreach (var cmd in eq.Cmds)
+            {
+                switch (cmd.Display.generic_type)
+                {
+                    case "FLAP_STATE":
+                        State = cmd.Value;
+                        //Position.Value =cmd.Value;
+                        break;
+
+                    case "FLAP_SLIDER":
+                        Position =cmd;
+                        break;
+
+                    case "FLAP_DOWN":
+                        Down = cmd;
+                        break;
+
+                    case "FLAP_STOP": 
+                        Stop = cmd;
+                        break;
+
+                    case "FLAP_UP": 
+                        Up = cmd;
+                        break;
+                        
+                }
+            }
+
+            _DataContextLoaded = true;
+        }
+        #endregion Private Methods
+    }
+}
