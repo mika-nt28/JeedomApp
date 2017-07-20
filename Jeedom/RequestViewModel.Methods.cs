833844c477a50917b70e9d8c691488f48516f3eb
Halbout Michael <michaelhalbout@gmail.com>
2017-07-20 12:40:48 +0200

    Update RequestViewModel.Methods.cs

--- Jeedom/RequestViewModel.Methods.cs
+++ Jeedom/RequestViewModel.Methods.cs
@@ -572,8 +572,8 @@ namespace Jeedom
 
         public async Task<bool> SendPosition(string position)
         {
-            var httpRpcClient = new HttpRpcClient("/plugins/geoloc/core/api/jeeApi.php?api=" + config.ApiKey + "&id=" + config.GeolocObjectId + "&value=" + position);
-
+            //var httpRpcClient = new HttpRpcClient("/plugins/geoloc/core/api/jeeGeoloc.php?apikey=" + config.ApiKey + "&id=" + config.GeolocObjectId + "&value=" + position);
+            var httpRpcClient = new HttpRpcClient("core/api/jeeApi.php?api=" + config.ApiKey + "&type=geoloc&id=" + config.GeolocObjectId + "&value=" + position);
             return await httpRpcClient.SendRequest();
         }
 
